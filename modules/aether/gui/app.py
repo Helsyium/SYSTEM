227850cc7812288b3306e19e71105404007e9355
Helsyium <ahmethakandastan@gmail.com>
2025-12-07 15:27:06 +0300

    Fix: Improve WAN connectivity by expanding STUN server list

--- modules/aether/gui/app.py
+++ modules/aether/gui/app.py
@@ -274,7 +274,10 @@ class AetherApp(ctk.CTkFrame):
             "stun:stun.l.google.com:19302",
             "stun:stun1.l.google.com:19302",
             "stun:stun2.l.google.com:19302",
-            "stun:stun.services.mozilla.com:3478"
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302",
+            "stun:stun.services.mozilla.com:3478",
+            "stun:global.stun.twilio.com:3478"
         ]
         
         config = RTCConfiguration(
